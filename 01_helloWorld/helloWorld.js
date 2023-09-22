0331e2349fbf28dbebbb57940b565bdddb2557bd
OdinProjectStudent <92232679+KushBh@users.noreply.github.com>
2023-09-21 21:10:04 -0700

    Update helloWorld.js

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,9 +1,5 @@
 const helloWorld = function() {
-<<<<<<< HEAD
   return 'Hello, World';
-=======
-  return 'Hello, World!';
->>>>>>> cdf2418fa55ca0639725ae6ee886a45846b67506
 };
 
 module.exports = helloWorld;
